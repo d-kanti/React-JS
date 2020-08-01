63b9aac62b9fb46a578356cf9d33be8d90f18dd2
d-kanti <dibyakanti22@gmail.com>
2020-08-02 00:44:21 +0530

    hi-there

---
+++ src/Header.js
@@ -0,0 +1,16 @@
+
+//es-7 snipest 'rfce'  shortcut---
+
+
+import React from 'react'
+
+function Header() {
+    return (
+        <div className= "Header">
+            <h1 color='red'> Hi there....</h1>
+            
+        </div>
+    )
+}
+
+export default Header
